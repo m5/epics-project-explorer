4af7ac19c8e233b287cf081a5d7c636fc793f338
m5 <mfivecoa@purdue.edu>
2009-09-23 00:36:09 -0400

    Set up testing and added a couple initial tests

--- epicsrec/tests/__init__.py
+++ epicsrec/tests/__init__.py
@@ -14,23 +14,40 @@ from paste.script.appinstall import SetupCommand
 from pylons import config, url
 from routes.util import URLGenerator
 from webtest import TestApp
+from epicsrec import model
 
 import pylons.test
+import sqlalchemy as sa
+import os
 
-__all__ = ['environ', 'url', 'TestController']
+__all__ = ['environ', 'url', 'TestController', 'TestModel']
 
 # Invoke websetup with the current config file
 SetupCommand('setup-app').run([config['__file__']])
 
+here_dir = os.path.dirname(os.path.abspath(__file__))
+conf_dir = os.path.dirname(os.path.dirname(here_dir))
+
 environ = {}
 
-class TestController(TestCase):
+class TestModel(TestCase):
+    """
+    We want the database to be created from scratch
+    before each test, and dropped after each test
+    """
+    def setUp(self):
+        model.meta.metadata.bind = model.meta.engine
+        model.meta.metadata.create_all()
+    def tearDown(self):
+        model.meta.metadata.drop_all()
+
+class TestController(TestModel):
 
     def __init__(self, *args, **kwargs):
         if pylons.test.pylonsapp:
             wsgiapp = pylons.test.pylonsapp
         else:
-            wsgiapp = loadapp('config:%s' % config['__file__'])
+            wsgiapp = loadapp('config:%s' %config['__file__'])
         self.app = TestApp(wsgiapp)
         url._push_object(URLGenerator(config['routes.map'], environ))
         TestCase.__init__(self, *args, **kwargs)
