c485d4e13b22dfa8ff405a03012f200433c9eeb0
m5 <mfivecoa@purdue.edu>
2009-10-15 19:50:59 -0400

    removed extranious files

--- build/lib/epicsrec/templates/xml_recs.mak
+++
@@ -1,5 +0,0 @@
-<response>
-%for rec in c.recs:
-	<rec name="${rec}">${rec}</rec>
-%endfor
-</response>
--- build/lib/epicsrec/tests/functional/test_choose.py
+++
@@ -1,7 +0,0 @@
-from epicsrec.tests import *
-
-class TestChooseController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='choose', action='index'))
-        # Test response...
